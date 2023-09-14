21fa5dce1cfd67e9322fba2b82413c58d69927b7
Kfja <jeremyaukf@gmail.com>
2023-09-15 01:08:48 +0800

    update on bizowner enquiry and trans hist style

--- src/pages/BizOwnerTransactionHist.vue
+++ src/pages/BizOwnerTransactionHist.vue
@@ -3,24 +3,28 @@
       <h1 class="text-2xl font-semibold mb-4">Transaction History</h1>
   
       <!-- Step 1: Filter by User Account -->
-      <div v-if="step === 1">
+      <div v-if="step === 1" class="mb-4">
         <!-- Display user account filter options -->
-        <button @click="proceedToStep(2)">Next</button>
+        <button @click="proceedToStep(2)" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+          Next
+        </button>
       </div>
   
       <!-- Step 2: Filter by Date Range -->
-      <div v-else-if="step === 2">
+      <div v-else-if="step === 2" class="mb-4">
         <label for="fromDate">From Date:</label>
-        <input type="date" id="fromDate" v-model="fromDate" />
-  
+        <input type="date" id="fromDate" v-model="fromDate" class="w-full px-3 py-2 rounded-md border border-gray-300" />
+
         <label for="toDate">To Date:</label>
-        <input type="date" id="toDate" v-model="toDate" />
-  
-        <button @click="proceedToStep(3)">Next</button>
+        <input type="date" id="toDate" v-model="toDate" class="w-full px-3 py-2 rounded-md border border-gray-300" />
+        <button @click="proceedToStep(3)" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        Next
+        </button>
       </div>
+
   
       <!-- Step 3: Display Filtered Transaction History -->
-      <div v-else-if="step === 3">
+      <div v-else-if="step === 3" class="mb-4">
         <h2 class="text-xl font-semibold mb-2">Filtered Transaction History</h2>
         <table class="min-w-full table-auto">
           <thead>
@@ -37,21 +41,27 @@
               <td class="border px-4 py-2">{{ transaction.customer }}</td>
               <td class="border px-4 py-2">{{ transaction.amount }}</td>
               <td class="border px-4 py-2">
-                <button @click="viewItemsInTransaction(transaction)">View Items</button>
+                <button @click="viewItemsInTransaction(transaction)" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                  View Items
+                </button>
               </td>
             </tr>
           </tbody>
         </table>
-        <button @click="resetSteps">Back to Step 1</button>
+        <button @click="resetSteps" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+          Back to Step 1
+        </button>
       </div>
   
       <!-- Step 4: View Items in Transaction (LINEITEM) -->
-      <div v-else-if="step === 4">
+      <div v-else-if="step === 4" class="mb-4">
         <h2 class="text-xl font-semibold mb-2">Items in Transaction</h2>
         <p v-if="selectedTransaction">
           Transaction ID: {{ selectedTransaction.id }}
         </p>
-        <button @click="resetSteps">Back to Step 1</button>
+        <button @click="resetSteps" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+          Back to Step 1
+        </button>
       </div>
     </div>
   </template>
@@ -105,5 +115,17 @@
   
   <style scoped>
   /* Add your component-specific styles here */
+  .bg-blue-500 {
+    background-color: #3490dc;
+  }
+  .text-white {
+    color: #fff;
+  }
+  .hover\:bg-blue-600:hover {
+    background-color: #2779bd;
+  }
+  .border {
+    border: 1px solid #e5e7eb;
+  }
   </style>
   
