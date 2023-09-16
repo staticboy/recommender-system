22d75fda71a41e4d48d3329eecf72ca8afd1fc44
Kfja <jeremyaukf@gmail.com>
2023-09-16 17:24:33 +0800

    update LoginModal.vue
    update LoginModal.vue

--- src/components/Modals/LoginModal.vue
+++ src/components/Modals/LoginModal.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 // import { useQuasar } from "quasar";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 // import { useAuthStore } from "src/stores/auth";
 
