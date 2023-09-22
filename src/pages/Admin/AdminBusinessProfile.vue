dddeb3094629e44ae5babaf3d93ab1e070b7faba
rktct <98218194+rktct@users.noreply.github.com>
2023-09-23 00:11:42 +0800

    Fix: Button Color

--- src/pages/Admin/AdminBusinessProfile.vue
+++ src/pages/Admin/AdminBusinessProfile.vue
@@ -143,12 +143,12 @@ table * {
 .btn,
 button {
   border-radius: 8px;
-  border: 1px solid transparent;
+  border: 1px solid black;
   padding: 0.6em 1.2em;
   font-size: 1em;
   font-weight: 500;
   font-family: inherit;
-  background-color: #1a1a1a;
+  background-color: white;
   cursor: pointer;
   transition: border-color 0.25s;
 }
