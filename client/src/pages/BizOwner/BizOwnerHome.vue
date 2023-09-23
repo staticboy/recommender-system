cb53bde7b3d8edf99e6970c4f3a7b25a43b5ea3d
staticboy <dehan1998@gmail.com>
2023-09-23 12:25:54 +0800

    fix: remove fullscreen class

--- client/src/pages/BizOwner/BizOwnerHome.vue
+++ client/src/pages/BizOwner/BizOwnerHome.vue
@@ -1,90 +1,92 @@
 <template>
-  <div class="fullscreen text-white text-center q-pa-md flex flex-center">
-    <div class="admin-hbody">
-      <h1>Business Owner Home</h1>
-      <section class="admin_jobs">
-        <ul class="flex">
-          <li>
-            <q-btn
-              class="q-mt-xl"
-              color="white"
-              text-color="blue"
-              unelevated
-              to="/biz/profile"
-              label="View Profile"
-              no-caps
-            />
-          </li>
-          <li>
-            <q-btn
-              class="q-mt-xl"
-              color="white"
-              text-color="blue"
-              unelevated
-              to="/biz/submit-enq"
-              label="Submit Enquiry"
-              no-caps
-            />
-          </li>
-          <li>
-            <q-btn
-              class="q-mt-xl"
-              color="white"
-              text-color="blue"
-              unelevated
-              to="/biz/trans-hist"
-              label="View Transaction History"
-              no-caps
-            />
-          </li>
-          <li>
-            <q-btn
-              class="q-mt-xl"
-              color="white"
-              text-color="blue"
-              unelevated
-              to="/biz/prod-lists"
-              label="View Product List"
-              no-caps
-            />
-          </li>
-          <li>
-            <q-btn
-              class="q-mt-xl"
-              color="white"
-              text-color="blue"
-              unelevated
-              to="/biz/chart"
-              label="View Chart"
-              no-caps
-            />
-          </li>
-          <li>
-            <q-btn
-              class="q-mt-xl"
-              color="white"
-              text-color="blue"
-              unelevated
-              to="/biz/products-all"
-              label="Add Product"
-              no-caps
-            />
-          </li>
-          <li>
-            <q-btn
-              class="q-mt-xl"
-              color="white"
-              text-color="blue"
-              unelevated
-              to="/biz/products"
-              label="View All Products"
-              no-caps
-            />
-          </li>
-        </ul>
-      </section>
+  <q-page>
+    <div class="text-center q-pa-md flex flex-center">
+      <div class="admin-hbody">
+        <h1>Business Owner Home</h1>
+        <section class="admin_jobs">
+          <ul class="flex">
+            <li>
+              <q-btn
+                class="q-mt-xl"
+                color="white"
+                text-color="blue"
+                unelevated
+                to="/biz/profile"
+                label="View Profile"
+                no-caps
+              />
+            </li>
+            <li>
+              <q-btn
+                class="q-mt-xl"
+                color="white"
+                text-color="blue"
+                unelevated
+                to="/biz/submit-enq"
+                label="Submit Enquiry"
+                no-caps
+              />
+            </li>
+            <li>
+              <q-btn
+                class="q-mt-xl"
+                color="white"
+                text-color="blue"
+                unelevated
+                to="/biz/trans-hist"
+                label="View Transaction History"
+                no-caps
+              />
+            </li>
+            <li>
+              <q-btn
+                class="q-mt-xl"
+                color="white"
+                text-color="blue"
+                unelevated
+                to="/biz/prod-lists"
+                label="View Product List"
+                no-caps
+              />
+            </li>
+            <li>
+              <q-btn
+                class="q-mt-xl"
+                color="white"
+                text-color="blue"
+                unelevated
+                to="/biz/chart"
+                label="View Chart"
+                no-caps
+              />
+            </li>
+            <li>
+              <q-btn
+                class="q-mt-xl"
+                color="white"
+                text-color="blue"
+                unelevated
+                to="/biz/products-all"
+                label="Add Product"
+                no-caps
+              />
+            </li>
+            <li>
+              <q-btn
+                class="q-mt-xl"
+                color="white"
+                text-color="blue"
+                unelevated
+                to="/biz/products"
+                label="View All Products"
+                no-caps
+              />
+            </li>
+          </ul>
+        </section>
+      </div>
     </div>
-  </div>
+  </q-page>
 </template>
 
 <script setup lang="ts"></script>
