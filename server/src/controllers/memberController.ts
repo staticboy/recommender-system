9c60b3b35214842fc8961489886618f28881a89f
Anne Muriele <annemurieletatactaperla@gmail.com>
2023-09-30 00:45:40 +0800

    feat: add controllers for member

--- server/src/controllers/memberController.ts
+++ server/src/controllers/memberController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 import { db } from '../../db';
 
+
+// GET 
+
 export async function getMemberAll(req: Request, res: Response) {
     try 
     {
@@ -44,4 +47,208 @@ export async function getMemberPreference(req: Request, res: Response) {
         res.status(500).json({ error: 'Internal server error' });
     } 
 }
+
+export async function getMemberTransactions(req: Request, res: Response) {
+    try 
+    {
+        const member = await db.any('SELECT * FROM mem_get_transactions($1)', [req.body]);
+        res.status(200).json(member);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    } 
+}
+
+// DELETE
+
+export async function deleteMemberPreferenceInfo(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_delete_preference_info($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Preference deleted successfully.' });
+        } else if (result === -1) {
+            res.status(404).json({ message: 'Preference record not found.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+export async function deleteMemberCartItem(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_delete_from_cart($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Cart item deleted successfully.' });
+        } else if (result === -1) {
+            res.status(404).json({ message: 'Cart item not found.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+export async function deleteMemberWishItem(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_delete_from_wishlist($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Wishlist item deleted successfully.' });
+        } else if (result === -1) {
+            res.status(404).json({ message: 'Wishlist item not found.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+// INSERT
+
+export async function insertPreferenceInfo(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_insert_preference_info($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Preference inserted successfully.' });
+        } else if (result === -1) {
+            res.status(500).json({ error: 'DB error: Preference insertion failed.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+export async function insertProdRating(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_insert_prod_rating($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Product rated successfully.' });
+        } else if (result === 0) {
+            res.status(500).json({ error: 'DB error: Prod raiting insert failed.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+export async function insertProdWishlist(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_insert_to_wishlist($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Wishlist item added successfully.' });
+        } else if (result === 0) {
+            res.status(500).json({ error: 'DB error: Prod wishlist insert failed.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+export async function insertNewMember(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_register_acct($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'New member added successfully.' });
+        } else if (result === -1) {
+            res.status(500).json({ error: 'Duplicate member email found.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+// UPDATE
+
+export async function updateMemberPreferenceInfo(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_update_preference_info($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Preference information updated successfully.' });
+        } else if (result === -1) {
+            res.status(500).json({ error: 'DB error: Preference update failed.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+export async function updateMemberProfileInfo(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem-update_profilee_info($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Profile information updated successfully.' });
+        } else if (result === -1) {
+            res.status(500).json({ error: 'DB error: Profile update failed.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+// LOGIM
+
+export async function memberLogin(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM mem_check_credentials($1)', [req.body]);
+
+        if (result === 1) {
+            res.status(200).json({ message: 'Welcome to Sportify.' });
+        } else if (result === -1) {
+            res.status(500).json({ error: 'Please check user input.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Oops, something broke.' });
+    }
+}
+
+
+
+
+
 
--- server/src/routes/memberRoutes.ts
+++ server/src/routes/memberRoutes.ts
@@ -1,11 +1,37 @@
 import { Router } from 'express';
-import { getMemberAll, getMemberById, getMemberByRegdate, getMemberPreference } from '../controllers/memberController';
+import { getMemberAll, 
+  getMemberById, 
+  getMemberByRegdate, 
+  getMemberPreference,
+  getMemberTransactions,
+  insertNewMember,
+  insertPreferenceInfo,
+  insertProdRating,
+  insertProdWishlist,
+  updateMemberPreferenceInfo,
+  updateMemberProfileInfo,
+  deleteMemberWishItem,
+  deleteMemberCartItem,
+  deleteMemberPreferenceInfo,
+  memberLogin } from '../controllers/memberController';
 
-  const router = Router();
+const router = Router();
 
 router.get('/getAll', getMemberAll);
 router.post('/getById', getMemberById);
 router.post('/getByDate', getMemberByRegdate);
 router.post('/getPreference', getMemberPreference);
+router.post('/getTransaction', getMemberTransactions);
+router.post('/addNewMember', insertNewMember);
+router.post('/addNewPref', insertPreferenceInfo);
+router.post('/addNewRating', insertProdRating);
+router.post('/addNewWishlist', insertProdWishlist);
+router.put('/editPref', updateMemberPreferenceInfo);
+router.put('/editProfile', updateMemberProfileInfo);
+router.put('/memberLogin', memberLogin);
+router.delete('/delWish', deleteMemberWishItem);
+router.delete('/delCart', deleteMemberCartItem);
+router.delete('/delPref', deleteMemberPreferenceInfo);
+
 
 export default router;
