120b71e0f2e5dff681b228364cb51b3c5ab1d6a7
JulioSoo <sooleong89@gmail.com>
2023-10-01 03:51:18 +0800

    feat : update category Controller & Routes

--- server/src/controllers/categoryController.ts
+++ server/src/controllers/categoryController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { db } from '../../db';
+import exp from 'constants';
 
 // GET 
 export async function getCategoryAll(req: Request, res: Response) {
@@ -11,4 +12,75 @@ export async function getCategoryAll(req: Request, res: Response) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error.' });
     } 
+}
+
+// POST
+
+export async function getCategoryById(req: Request, res: Response) {        
+    try 
+    {
+        const category = await db.one('SELECT * FROM cat_get_by_id($1)', [req.body]);
+        res.status(200).json(category);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    } 
+}
+
+export async function registerCategory(req: Request, res: Response) {
+    try 
+    {
+        const result = await db.one('SELECT * FROM cat_insert_new($1)', [req.body]);
+        
+        if (result.result_code === 1) {
+            res.status(200).json({ message: 'Category registered successfully' });
+        } else if (result.result_code === -1) {
+            res.status(500).json({ message: 'Category already exists' });
+        } else {
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Shit happen' });
+    } 
+}
+
+
+// UPDATE / PUT
+
+export async function updateCategoryDetails(req: Request, res: Response) {
+    try {
+        const result = await db.one('SELECT * FROM cat_update_details($1)', [req.body]);
+
+        if (result.cat_update_details === 1){
+            res.status(200).json({message: 'Category details updated'});
+        } else if (result.cat_update_details === -1){
+            res.status(404).json({ error: 'DB error: detail updates failed'})
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Shit happen' });
+    } 
+}
+
+// DELETE
+
+export async function deleteCategory(req: Request, res: Response) {
+    try {
+        const result = await db.one(
+            'SELECT * FROM cat_delete_by_id($1)', [req.body]);
+
+        if (result.cat_delete_by_id === 1) {
+            res.status(200).json({ message: 'Category deleted successfully.' });
+        } else if (result.cat_delete_by_id === -1) {
+            res.status(404).json({ message: 'Category record not found.' });
+        } else {
+            res.status(500).json({ error: 'Internal server error.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Shit happen.' });
+    }
 }
--- server/src/routes/categoryRoutes.ts
+++ server/src/routes/categoryRoutes.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
-import { getCategoryAll } from '../controllers/categoryController';
+import { getCategoryAll,
+  getCategoryById,
+  registerCategory,
+  updateCategoryDetails,
+  deleteCategory
+} from '../controllers/categoryController';
 
 const router = Router();
 
 router.get('/getCatAll', getCategoryAll);
+router.post('/getCatById', getCategoryById);
+router.post('/registerCategory', registerCategory);
+router.put('/updateCategory', updateCategoryDetails);
+router.delete('/deleteCategory', deleteCategory)
 
 export default router;
