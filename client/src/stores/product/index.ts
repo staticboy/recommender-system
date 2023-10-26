f0484e0f5ffe548e32da5be1edbac4d6c7f74a37
staticboy <dehan1998@gmail.com>
2023-10-27 07:20:45 +0800

    feat: product store init type and endpoint

--- client/src/stores/product/index.ts
+++ client/src/stores/product/index.ts
@@ -3,5 +3,16 @@ import { ref } from "vue";
 import axios, { AxiosResponse } from "axios";
 
 export const useProductStore = defineStore("product", () => {
-  
+  const getProductsByCategory = async (id: string) => {
+  };
+  const getProductsByBusiness = async (id: string) => {
+  };
+  const getProductByID = async (id: string) => {
+  };
+
+  return {
+    getProductsByCategory,
+    getProductsByBusiness,
+    getProductByID,
+  }
 });
---
+++ client/src/stores/product/types.ts
@@ -0,0 +1,13 @@
+export interface ProductDetails {
+  prod_id: string
+  prod_name: string,
+  prod_description: string,
+  prod_price: number,
+  prod_stockqty: number,
+  prod_modelnum: string,
+  cat_id: string,
+  sub_cat: string,
+  biz_id: string,
+  prod_dateadded: Date,
+  prod_status: string,
+}
